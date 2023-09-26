961de8b26f3d21025f68f8b4cb01baedee7db963
Chinyere offor <obiagelichinyere566@gmail.com>
2023-09-26 13:02:46 -0400

    completed

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import path from 'path'
+import GiftsController from '../controllers/gifts.js'
 
 import { fileURLToPath } from 'url'
 
@@ -10,9 +11,7 @@ const __dirname = path.dirname(__filename)
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json(giftData)
-})
+router.get('/', GiftsController.getGifts)
 
 router.get('/:giftId', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
